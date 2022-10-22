188f4b5cfe5565d8e5eb8ba51f63dbbc9df0fc57
yudax42 <yudax42@gmail.com>
2022-10-22 13:41:31 +0100

    feat: add AppButton component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <button
+    class="px-3 py-1.5 rounded bg-black text-white text-sm"
+    v-bind="$attrs"
+  >
+    <slot />
+  </button>
+</template>
--- src/pages/AuthPage.vue
+++ src/pages/AuthPage.vue
@@ -1,5 +1,14 @@
+<script lang="ts" setup>
+import AppButton from "@/components/AppButton.vue";
+
+const authorizeGithub = () => {
+  console.log("...");
+};
+</script>
+
 <template>
   <main>
     <h1 class="text-xl">Auth Page</h1>
+    <AppButton @click="authorizeGithub">Authorize my Github account</AppButton>
   </main>
 </template>
