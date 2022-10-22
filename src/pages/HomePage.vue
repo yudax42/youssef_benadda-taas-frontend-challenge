d9cd9d99446e44cb0f99a76f80b6742d2388523c
yudax42 <yudax42@gmail.com>
2022-10-22 19:31:29 +0100

    feat: show selected repo

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,7 +4,7 @@ import { storeToRefs } from "pinia";
 import useRepoStore from "@/stores/repository";
 
 const repoStore = useRepoStore();
-let { repos } = storeToRefs(repoStore);
+let { repos, currentRepo } = storeToRefs(repoStore);
 
 onBeforeMount(async () => {
   await repoStore.getRepos();
@@ -14,10 +14,18 @@ onBeforeMount(async () => {
 <template>
   <main>
     <h1 class="text-xl">Home Page</h1>
-    <ul>
-      <li v-for="repo in repos" :key="repo.id">
-        {{ repo.name }}
-      </li>
-    </ul>
+    <div class="grid grid-cols-2 gap-2">
+      <ul>
+        <li
+          v-for="repo in repos"
+          :key="repo.id"
+          @click="repoStore.setCurrentRepo(repo.id)"
+        >
+          {{ repo.name }}
+        </li>
+      </ul>
+
+      <span>{{ currentRepo }}</span>
+    </div>
   </main>
 </template>
