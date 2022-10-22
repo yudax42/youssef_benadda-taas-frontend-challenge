fd2bc3328bd38271eb4766eafdedf0264a725cb0
yudax42 <yudax42@gmail.com>
2022-10-22 14:08:55 +0100

    fix: add github authorization url to config

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,9 @@ const config = {
   apiBaseURL: "https://api.github.com",
   clientId: "Iv1.56cfb8535f2c1de3",
   redirectUri: "http://localhost:5173/oauth/callback",
+  authorizationUrl: function () {
+    return `${this.baseURL}/login/oauth/authorize?client_id=${this.clientId}&scope=repo&redirect_uri=${this.redirectUri}`;
+  },
 };
 
 export default config;
