5200d9996020181ceaab4972509aa32103a51ef4
yudax42 <yudax42@gmail.com>
2022-10-22 19:27:56 +0100

    fix: fix dynamic redirectUri origin

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,14 @@
 const mode = import.meta?.env?.MODE || "development";
+const origin =
+  typeof window !== "undefined"
+    ? window?.location?.origin
+    : "http://localhost:5173";
 
 const config = {
   baseURL: "https://github.com",
   apiBaseURL: "https://api.github.com",
   clientId: "Iv1.56cfb8535f2c1de3",
-  redirectUri: "http://localhost:5173/oauth/callback",
+  redirectUri: `${origin}/oauth/callback`,
   authorizationUrl: function () {
     return `${this.baseURL}/login/oauth/authorize?client_id=${this.clientId}&scope=repo&redirect_uri=${this.redirectUri}`;
   },
