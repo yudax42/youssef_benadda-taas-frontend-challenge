6b478c8019d0876e4ec80d9172c131eca6373cdc
yudax42 <yudax42@gmail.com>
2022-10-23 15:31:17 +0100

    feat: add listbox component

---
+++ src/components/AppListbox.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { defineProps, ref } from "vue";
+
+let emit = defineEmits(["update"]);
+
+interface ListBox {
+  selected: string | null;
+  options: Record<string, string>[];
+}
+const props = defineProps<ListBox>();
+
+function selectOption(event: any) {
+  emit("update", event.target.value);
+}
+</script>
+
+<template>
+  <div>
+    <select @change="selectOption" name="" id="">
+      <option
+        v-for="(option, i) in props.options"
+        :key="i"
+        :value="option.value"
+        :selected="option.value === selected"
+      >
+        {{ option.name }}
+      </option>
+    </select>
+  </div>
+</template>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,13 +1,28 @@
-<script lang="ts" setup>
-import { onBeforeMount } from "vue";
+<script lang="ts" async setup>
+import { onBeforeMount, computed } from "vue";
 import { storeToRefs } from "pinia";
 import useRepoStore from "@/stores/repository";
+import AppListBox from "@/components/AppListbox.vue";
 
 const repoStore = useRepoStore();
 let { repos, branches, commits, currentBranch } = storeToRefs(repoStore);
 
 onBeforeMount(async () => {
   await repoStore.getRepos();
+});
+
+const selectBranch = (branch: string) => {
+  console.log("select branch", branch);
+  repoStore.setCurrentBranch(branch);
+};
+
+const branchesList = computed(() => {
+  return branches.value.map((branch) => {
+    return {
+      name: branch.name,
+      value: branch.commit.sha,
+    };
+  });
 });
 </script>
 
@@ -26,15 +41,12 @@ onBeforeMount(async () => {
       </ul>
       <div>
         <h4 class="text-xl font-bold">Branches</h4>
-        <ul>
-          <li
-            v-for="branch in branches"
-            :key="branch.name"
-            @click="repoStore.setCurrentBranch(branch.commit.sha)"
-          >
-            {{ branch.name }}
-          </li>
-        </ul>
+        <AppListBox
+          v-if="branches"
+          :selected="currentBranch"
+          :options="branchesList"
+          @update="selectBranch"
+        />
 
         <h4 class="text-xl font-bold">Commits</h4>
         <ul>
