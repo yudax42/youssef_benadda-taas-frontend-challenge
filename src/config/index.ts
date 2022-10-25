9550f9689daa04fe5c85949cfd2ec451264007de
yudax42 <yudax42@gmail.com>
2022-10-25 14:43:07 +0100

    refactor: getData and fix hard coded values in the config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,17 +1,21 @@
-const origin =
-  typeof window !== "undefined"
-    ? window?.location?.origin
-    : "https://youcan.yudax.me";
+const devProxy = "http://localhost:1337/api/getToken";
+let origin: string | null = null;
+
+// to fix window is not defined error in cf worker
+if (typeof window !== "undefined") {
+  origin = window?.location?.origin;
+}
+
+function proxyBaseUrl() {
+  return origin?.includes("localhost") ? devProxy : "/api/getToken";
+}
 
 const config = {
   clientId: "Iv1.56cfb8535f2c1de3",
   baseURL: "https://github.com",
   apiBaseURL: "https://api.github.com",
   redirectUri: `${origin}/oauth/callback`,
-  proxy:
-    origin === "https://youcan.yudax.me"
-      ? "/api/getToken"
-      : "http://localhost:1337/api/getToken",
+  proxy: proxyBaseUrl(),
   authorizationUrl: function () {
     return `${this.baseURL}/login/oauth/authorize?client_id=${this.clientId}&scope=repo&redirect_uri=${this.redirectUri}`;
   },
--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -3,25 +3,26 @@
  * @param request
  * @returns Promise<any>
  */
+
 async function getData(request: Request): Promise<any> {
-  const { headers } = request;
-  const contentType = headers.get("content-type") || "";
-  if (contentType.includes("application/json")) {
-    return await request.json();
-  } else if (contentType.includes("application/text")) {
-    return request.text();
-  } else if (contentType.includes("text/html")) {
-    return request.text();
-  } else if (contentType.includes("form")) {
-    const formData = await request.formData();
-    const body: Record<string, any> = {};
-    for (const entry of Object(formData).entries()) {
-      const key = entry[0];
-      body[key] = entry[1];
-    }
-    return body;
+  const contentType = request.headers.get("content-type") || "";
+
+  switch (contentType) {
+    case "application/json":
+      return await request.json();
+    case "application/text" || "text/html":
+      return request.text();
+    case "form":
+      const formData = await request.formData();
+      const body: Record<string, any> = {};
+      for (const entry of Object(formData).entries()) {
+        const key = entry[0];
+        body[key] = entry[1];
+      }
+      return body;
+    default:
+      return null;
   }
-  return null;
 }
 
 export default getData;
